3d7350067724ee8d7df63eb55e3782c2e1e1d919
BlackRece <blackrece@gmail.com>
2026-01-21 19:25:37 +0000

    added timer for future use to spawn enemies after a delay.
    added library of enemy prefabs
    
    added an array for use as a pool of instantiated enemies
    
    added area var to store bounds of player to use for spawning enemies
    
    added logic to spawn an enemy when there are none

--- space-pang/Scripts/EnemyManager.cs
+++ space-pang/Scripts/EnemyManager.cs
@@ -5,18 +5,34 @@ namespace SpacePang.Scripts
 {
 	public partial class EnemyManager : Node
 	{
-		//private Dictionary<EnemyTypes, Node> _enemyPool { get; set; } = [];
+		private Timer _timer;
 
-		
+		private EnemyLibrary _lib;
+		//private Dictionary<EnemyTypes, Node> _enemyPool { get; set; } = [];
+		private List<Node> _enemies = [];
+		private Rect2 _area;
 		
 		// Called when the node enters the scene tree for the first time.
 		public override void _Ready()
 		{
+			_lib = GetNode<EnemyLibrary>("EnemyLibrary");
+			
+			_timer = GetNode<Timer>("EnemyTimer");
+			_timer.Start(0.1);
+
+			_area = GetParent().GetViewport().GetVisibleRect();
 		}
 
 		// Called every frame. 'delta' is the elapsed time since the previous frame.
 		public override void _Process(double delta)
 		{
+			if (_enemies.Count <= 1)
+			{
+				var enemy = _lib.GetEnemy(EnemyLibrary.EnemyTypes.Basic);
+				enemy.Position = new Vector2(_area.Size.X / 2, 50.0f);
+				_enemies.Add(enemy);
+				AddChild(enemy);
+			}
 		}
 	}
 }
