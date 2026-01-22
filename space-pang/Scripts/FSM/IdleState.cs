87281b3c97d25221c44da67999e26a5c0629d99a
BlackRece <blackrece@gmail.com>
2026-01-22 22:51:32 +0000

    added IdleState.cs

---
+++ space-pang/Scripts/FSM/IdleState.cs
@@ -0,0 +1,15 @@
+using Godot;
+
+namespace SpacePang.Scripts.FSM;
+
+public class IdleState : State
+{
+    public IdleState(Area2D agent, Area2D target) : base(agent, target)
+    {
+    }
+
+    public override void Go(double delta)
+    {
+        return;
+    }
+}
