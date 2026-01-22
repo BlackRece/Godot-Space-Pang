3658c885b37778dda75b0b1261d83e6f3b3ec7ac
BlackRece <blackrece@gmail.com>
2026-01-22 22:45:03 +0000

    separated state base class

--- space-pang/Scripts/FSM/FuzzyStateMachine.cs
+++ space-pang/Scripts/FSM/FuzzyStateMachine.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using Godot;
 
 namespace SpacePang.Scripts.FSM;
 
@@ -13,29 +12,4 @@ public sealed class FuzzyStateMachine
     }
     
     public Dictionary<string, State> States { get; private set; }
-}
-
-public abstract class State
-{
-    protected Area2D _agent; // Reference to the enemy using this state
-    private Area2D _target;
-    
-    public State(Area2D agent, Area2D target)
-    {
-        _agent = agent;
-        _target = target;
-    }
-    
-    // Called when entering the state
-    public virtual void Enter()
-    {
-    }
-    
-    // Called each frame while in this state
-    public abstract void Go(double delta);
-    
-    // Called when exiting the state
-    public virtual void Exit()
-    {
-    }
 }
---
+++ space-pang/Scripts/FSM/State.cs
@@ -0,0 +1,28 @@
+using Godot;
+
+namespace SpacePang.Scripts.FSM;
+
+public abstract class State
+{
+    protected Area2D _agent; // Reference to the enemy using this state
+    private Area2D _target;
+    
+    public State(Area2D agent, Area2D target)
+    {
+        _agent = agent;
+        _target = target;
+    }
+    
+    // Called when entering the state
+    public virtual void Enter()
+    {
+    }
+    
+    // Called each frame while in this state
+    public abstract void Go(double delta);
+    
+    // Called when exiting the state
+    public virtual void Exit()
+    {
+    }
+}
