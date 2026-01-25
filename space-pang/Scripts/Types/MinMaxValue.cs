14ecf3ce9ba2fe9da7da9c503ef410e2ae37a0d3
BlackRece <blackrece@gmail.com>
2026-01-25 17:56:57 +0000

    documented MinMaxValue.cs so that comments show up when using the type

--- space-pang/Scripts/Types/MinMaxValue.cs
+++ space-pang/Scripts/Types/MinMaxValue.cs
@@ -4,18 +4,38 @@ namespace SpacePang.Scripts.Types;
 
 public sealed record MinMaxValue<T> where T : INumber<T>
 {
+    /// <summary>
+    /// Minimum Value
+    /// </summary>
     public T Min { get; set; }
+    
+    /// <summary>
+    /// Maximum Value
+    /// </summary>
     public T Max { get; set; }
+    
+    /// <summary>
+    /// Threshold Value
+    /// </summary>
     public T Mid { get; set; }
 
     private T _current;
 
+    /// <summary>
+    /// Current value that is clamped between Min and Max.
+    /// </summary>
     public T Current
     {
         get => _current;
         set => _current = value > Max ? Max : value < Min ? Min : value;
     }
 
+    /// <summary>
+    /// Create a new type
+    /// </summary>
+    /// <param name="min">Minimum value</param>
+    /// <param name="max">Maximum value</param>
+    /// <param name="mid">Threshold value used for Current's starting value</param>
     public MinMaxValue(T min, T max, T mid)
     {
         Min = min;
@@ -24,6 +44,13 @@ public sealed record MinMaxValue<T> where T : INumber<T>
         Current = Mid;
     }
     
+    /// <summary>
+    /// Returns true if Current more than Mid
+    /// </summary>
     public bool IsAbove => Current > Mid;
+    
+    /// <summary>
+    /// Returns true if Current less than Mid
+    /// </summary>
     public bool IsBelow => Current < Mid;
 }
