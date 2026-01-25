95792e72c319e602c21f407bf72213618e9e0d99
BlackRece <blackrece@gmail.com>
2026-01-25 23:10:05 +0000

    re-added override to ToBeActivated() function based on current and last position of agent.
    no longer needs velocity
    
    next step: flesh out Entity class to contain variables require by states of state machine, including steering behaviours and player class

--- space-pang/Scripts/FSM/IdleState.cs
+++ space-pang/Scripts/FSM/IdleState.cs
@@ -4,16 +4,17 @@ namespace SpacePang.Scripts.FSM;
 
 public class IdleState : State
 {
+    private Vector2 lastPos = new Vector2(); 
     public IdleState(Area2D agent, Area2D target) : base(agent, target)
     {
     }
 
-    /*
     public override bool ToBeActivated()
     {
-        _agent.Ma
+        var hasNotMoved = lastPos.DistanceSquaredTo(_agent.Position) <= 0f;
+        lastPos = _agent.Position;
+        return hasNotMoved;
     }
-    */
 
     public override void Go(double delta)
     {
