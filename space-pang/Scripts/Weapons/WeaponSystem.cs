395fb154604572df8bfb1c7404f1e91fb4c2a96f
BlackRece <blackrece@gmail.com>
2026-02-24 21:43:44 +0000

    draghted ideas for weapon system

---
+++ space-pang/Scripts/Weapons/WeaponSystem.cs
@@ -0,0 +1,31 @@
+namespace SpacePang.Scripts.Weapons;
+
+public class WeaponSystem
+{
+    /*
+     * WeaponSystem:
+     * Base/Frame
+     * Fire Types: Kinetic, Explosive, Energetic
+     * 
+     * Barrel/Emitter
+     * Barrel Upgrades:
+     * - multi-direction: 1-way, 3-way, 5-way
+     * - multiple shots: single, double, quad
+     * 
+     * Chambers:
+     * - Kinetic: bullet, slow bullet, DOT bullet
+     * - Explosive: mines, dumb rocket, homing missile
+     * - Energetic: plasma ball, laser beam, chain lightning
+     *
+     * Drops:
+     * - BasicEnemies = scrap (currency)
+     * - Elites = parts/components
+     * - Mini Bosses = scrap and parts
+     * - Bosses = scrap and full random weapons
+     *
+     * WeaponBuilder:
+     * Button to open second screen, pauses game
+     * drag and drop parts from inventory into place?
+     * purchase parts using scrap
+     */
+}
